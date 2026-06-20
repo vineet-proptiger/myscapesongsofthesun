ef3cfb7387485ceaf8ac33d96af76bd1f920e88e
vineet-proptiger <vineet.mishra@proptiger.com>
2026-06-20 12:46:44 +0530

    some fixes

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -26,7 +26,7 @@ const Footer = () => (
       
       {/* RERA Block */}
       <div style={{ display: 'inline-block', background: 'rgba(255,255,255,0.05)', padding: '10px 24px', borderRadius: '4px', border: '1px solid rgba(255,255,255,0.1)' }}>
-        <span style={{ fontFamily: F_SANS, fontSize: '14px', color: '#aaa' }}>RERA No : </span>
+        <span style={{ fontFamily: F_SANS, fontSize: '14px', color: '#aaa' }}>RERA No :</span>
         <strong style={{ fontFamily: F_JOST, fontSize: '15px', color: '#fff', letterSpacing: '0.04em' }}>P02400008721</strong>
       </div>
     </div>
